f2782a4bd9db1aabc7db47090ac62460be756c6e
Rubén Ausín <ruben.ausin@holaglow.com>
2025-02-24 19:42:59 +0100

    GeneratedAnimations start

---
+++ animm/src/app/services/GeneratedAnimationsService.ts
@@ -0,0 +1,37 @@
+import { ApiCollection, ApiCollections } from "@/types/collections";
+import { GeneratedAnimation } from "@/types/generatedAnimations";
+
+export const genera = {
+  get: async(id: string): Promise<GeneratedAnimation | undefined> => {
+    const response =  await fetch(
+      process.env.API_URL + "/animations/" + id
+    );
+    if (!response.ok) {
+      return undefined;
+    }
+
+    return await response.json();
+  },
+  getAll: async(): Promise<GeneratedAnimation[]> => {
+    const response =  await fetch(
+      process.env.API_URL + "/animations/"
+    );
+    if (!response.ok) {
+      return [];
+    }
+
+    return await response.json();
+  },
+  add: async(generatedAnimation: GeneratedAnimation) => {
+    const response =  await fetch(
+      process.env.API_URL + "/animations/",
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(generatedAnimation)
+      }
+    );
+  }
+};
---
+++ animm/src/types/generatedAnimations.ts
@@ -0,0 +1,41 @@
+import { Module, ModuleTypeEnum, Template, TemplateImage, TemplateVariable } from "./collections";
+
+
+export interface GeneratedAnimation{
+  baseTemplateId: number;
+  baseTemplate: Template;
+  name: string;
+  image: string;
+  folder: string;
+  status: GeneraetdAnimationStatusEnum;
+  modules: GeneratedModule[];
+}
+
+export interface GeneratedModule{
+  moduleType: ModuleTypeEnum;
+  baseModuleId: number;
+  baseModule: Module;
+  file:string;
+  variables: GeneratedTemplateVariable[];
+  images: GeneratedTemplateImage[];
+}
+
+export interface GeneratedTemplateImage{
+  tepmlateImageId: number;
+  templateImage: TemplateImage;
+  image: string;
+}
+
+export interface GeneratedTemplateVariable{
+  tepmlateVariableId: number;
+  templateVariable: TemplateVariable;
+  value: string;
+}
+
+export enum GeneraetdAnimationStatusEnum{
+  NoStatus,
+  ToReview,
+  Reviewed,
+  Approved,
+  OnHold
+}
